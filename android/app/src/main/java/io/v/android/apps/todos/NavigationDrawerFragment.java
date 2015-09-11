e50f72f2a31f24df80716475fe2306077ad781d6
Adam Sadovsky <sadovsky@google.com>
2015-09-10 17:43:00 -0700

    TBR everything: run `v23 copyright fix` to regen VERSION file
    Change-Id: If2f52a518dbcc88b289984b5f1b64cd280c7560a

--- android/app/src/main/java/io/v/android/apps/todos/NavigationDrawerFragment.java
+++ android/app/src/main/java/io/v/android/apps/todos/NavigationDrawerFragment.java
@@ -1,3 +1,7 @@
+// Copyright 2015 The Vanadium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package io.v.android.apps.todos;
 
 import android.support.v7.app.AppCompatActivity;
